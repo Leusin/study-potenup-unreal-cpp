2ef3b477e3d4fbe2a9fed893401e6b48cc2a5f11
Leusin <leusin@naver.com>
2025-10-14 12:13:26 +0900

    work: 언리얼 구조체, TMap, TMultiMap 실습

--- UEPart1/UnrealContainer/Source/UnrealContainer/MyGameInstance.cpp
+++ UEPart1/UnrealContainer/Source/UnrealContainer/MyGameInstance.cpp
@@ -4,6 +4,25 @@
 #include "MyGameInstance.h"
 #include "Algo/Accumulate.h"
 
+// 랜덤으로 이름 만드는 함수
+static FString MakeRandomName()
+{
+	static TCHAR FirstChar[] = TEXT("김이박최");
+	static TCHAR MiddkeChar[] = TEXT("가나다라");
+	static TCHAR LastChar[] = TEXT("마바사아");
+
+	// 랜덤 이름 생성
+	TArray<TCHAR> RandomArray;
+	RandomArray.SetNum(3);
+	RandomArray[0] = FirstChar[FMath::RandRange(0, 3)];;
+	RandomArray[1] = MiddkeChar[FMath::RandRange(0, 3)];;
+	RandomArray[2] = LastChar[FMath::RandRange(0, 3)];;
+
+	//생성된 이름 반환
+	return RandomArray.GetData();
+}
+
+
 void UMyGameInstance::Init()
 {
 	Super::Init();
@@ -38,7 +57,7 @@ void UMyGameInstance::Init()
 
 
 	// 비교 배열 선언
-	int32 CArray[]  = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
+	int32 CArray[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
 	TArray<int32> Int32ArrayComprare;
 
 	// 비교 메모리의 초기 크기 지정
@@ -67,9 +86,88 @@ void UMyGameInstance::Init()
 	{
 		Int32Set.Add(ArrayNum - i);
 	}
-	
+
 	Int32Set.Remove(2);
 	Int32Set.Remove(4);
 	Int32Set.Remove(6);
 
+
+	/// 언리얼 구조체
+
+	// 학생 데이터 생성
+	const int32 StudentNum = 300;
+	for (int32 i = 0; i < StudentNum; i++)
+	{
+		StudentsData.Emplace(FStudentData(MakeRandomName(), i));
+	}
+
+	// 학생 데이터를 TArray로 변환(Transform)
+	TArray<FString> AllStudentsName;
+	Algo::Transform(StudentsData, AllStudentsName,
+		[](const FStudentData& Val)
+		{
+			return Val.Name;
+		}
+	);
+
+	// 이름 몇 개인지 출력
+	UE_LOG(LogTemp, Log, TEXT("모든 학생 이름의 수 : %d"), AllStudentsName.Num());
+
+	/*
+		LogTemp: 모든 학생 이름의 수 : 300
+	*/
+
+
+	/// 언리얼 TMap
+
+	// 이름을 키로 갖는 Map 생성
+	TMap<FString, int32> StudentsMapByUniqueName;
+	Algo::Transform(StudentsData, StudentsMapByUniqueName,
+		[](const FStudentData& Val)
+		{
+			return TPair<FString, int32>(Val.Name, Val.Order);
+		});
+
+	UE_LOG(LogTemp, Log, TEXT("고유한 학생 이름 수 : %d"), StudentsMapByUniqueName.Num());
+
+	/*
+		LogTemp: 고유한 학생 이름 수 : 64
+	*/
+
+	const FString TargetName(TEXT("김가마"));
+	int32* Order = StudentsMapByUniqueName.Find(TargetName);
+
+	if (Order)
+	{
+		UE_LOG(LogTemp, Log, TEXT("%s 이름을 가진 학생 순번 %d"), *TargetName, *Order);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Log, TEXT("%s 학생 없음"), *TargetName);
+	}
+
+	/*
+		LogTemp: 김가마 이름을 가진 학생 순번 193
+	*/
+
+	/// 언리얼 TMultiMap
+	TMultiMap<FString, int32> StudentsMapByName;
+	
+	// 변환
+	Algo::Transform(StudentsData, StudentsMapByName,
+		[](const FStudentData& Val)
+		{
+			return TPair<FString, int32>(Val.Name, Val.Order);
+		});
+
+	// 검색.
+	TArray<int32> AllOrders;
+	StudentsMapByName.MultiFind(TargetName, AllOrders);
+
+	// 검색된 학생 수 출력.
+	UE_LOG(LogTemp, Log, TEXT("이름이 %s인 학생 수: %d"), *TargetName, AllOrders.Num());
+
+	/*
+		LogTemp: 이름이 김가마인 학생 수: 5
+	*/
 }
--- UEPart1/UnrealContainer/Source/UnrealContainer/MyGameInstance.h
+++ UEPart1/UnrealContainer/Source/UnrealContainer/MyGameInstance.h
@@ -6,6 +6,32 @@
 #include "Engine/GameInstance.h"
 #include "MyGameInstance.generated.h"
 
+// 구조체 선언
+USTRUCT(Atomic, BlueprintType/*,NoExport*/)
+struct FStudentData
+{
+	GENERATED_BODY()
+
+	FStudentData()
+		: Name(TEXT("김주영")), Order(-1)
+	{
+
+	}
+
+	FStudentData(const FString& InName, int32 InOrder)
+		: Name(InName), Order(InOrder)
+	{
+
+	}
+
+public:
+	UPROPERTY()
+	FString Name;
+
+	UPROPERTY()
+	int32 Order;
+};
+
 /**
  * 
  */
@@ -17,4 +43,11 @@ class UNREALCONTAINER_API UMyGameInstance : public UGameInstance
 public:
 	virtual void Init() override;
 
+private:
+	TArray<FStudentData> StudentsData;
+
+	UPROPERTY()
+	TArray<TObjectPtr<class UStudent>> Students;
+
+	TMap<int32, FString> StudentsMap;
 };
